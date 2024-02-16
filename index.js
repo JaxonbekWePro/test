da0423d62cde478c8b1a6156e026d8ecffc1431f
jaxonbek <jaxonbekmusinov@gmail.com>
2024-02-16 19:49:17 +0500

    "first commit"

---
+++ index.js
@@ -0,0 +1,19 @@
+let name = "Alex"
+let user_name = prompt("what is your name?")
+
+
+user_account = prompt("номер счета?")
+let account = 7777
+let user_money = prompt("сколько обналичить?")
+let money = 10000
+if (name.includes(user_name)) {
+    alert("номер счета")
+} else {
+    alert("Пользователь не найден")
+
+}
+if (money.includes(user_money)) {
+    alert("все отлично")
+} else {
+    ("недостаточно средств")
+}
